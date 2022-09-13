bcc4de6a47239a9f61513e6120729247b1b57979
Marcelo1197 <sanchez.marcelo2010@gmail.com>
2022-09-13 10:48:10 -0300

    FIX: agrega Console.ReadKey()
    Mantiene abierta la consola, hasta que se presione una tecla

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -11,6 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadKey(); //Mantiene abierta la consola (lee la tecla que presionemos)
         }
     }
 }
