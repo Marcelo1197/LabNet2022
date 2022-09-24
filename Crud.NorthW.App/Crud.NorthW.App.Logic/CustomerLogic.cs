c143a8397fbdba1bbbb9166fd771aabaf953bb6c
Marcelo1197 <sanchez.marcelo2010@gmail.com>
2022-09-24 17:54:32 -0300

    OK: Primer commit, funciona
    Proyecto funcionando, muestra lista de customers

---
+++ Crud.NorthW.App/Crud.NorthW.App.Logic/CustomerLogic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Crud.NorthW.App.Entities;
+using Crud.NorthW.App.Data;
+
+namespace Crud.NorthW.App.Logic
+{
+    public class CustomerLogic
+    {
+        private NorthwindContext _context;
+        
+        public CustomerLogic()
+        {
+            _context = new NorthwindContext();
+        }
+
+        public List<Customers> GetAll()
+        {
+            return _context.Customers.ToList();
+        }
+    }
+}
---
+++ Crud.NorthW.App/Crud.NorthW.App.UI/Program.cs
@@ -0,0 +1,26 @@
+﻿using Crud.NorthW.App.Entities;
+using Crud.NorthW.App.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crud.NorthW.App.UI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mostramos lista de Customers");
+            CustomerLogic cl = new CustomerLogic();
+            List<Customers> customersList = cl.GetAll();
+
+            foreach (var customer in customersList)
+            {
+                Console.WriteLine($"\nID: {customer.CustomerID}\nCompañia: {customer.CompanyName}\nNombre de contacto:{customer.ContactName}\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
