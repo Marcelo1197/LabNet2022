e70c4c7c090cc927bbdfb52851de278020580e4c
Marcelo1197 <sanchez.marcelo2010@gmail.com>
2022-10-08 14:29:37 -0300

    OK: agrega primer commit. Requerimientos cumplidos
    App funcional, requerimientos básicos cumplidos.

---
+++ Crud.NorthW.App/Crud.NorthW.App.MVC/Validations/ShippersInputValidation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Crud.NorthW.App.MVC.Validations
+{
+    public class ShippersInputValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                string message = value.ToString();
+            }
+
+            return base.IsValid(value, validationContext);
+        }
+    }
+}
